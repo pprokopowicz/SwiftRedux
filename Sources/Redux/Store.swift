5e3720d85ac0e54e3eb0d53a2c631c13ec416949
Piotr Prokopowicz <p.prokopowicz1@gmail.com>
2020-06-09 20:25:54 +0200

    Public inits and methods fix

--- Sources/Redux/Store.swift
+++ Sources/Redux/Store.swift
@@ -11,12 +11,12 @@ public class Store<StoreState: State> {
     private var state: StoreState
     private var subscribers: [StoreSubscriber] = []
     
-    init(state: StoreState, reducer: @escaping Reducer<StoreState>) {
+    public init(state: StoreState, reducer: @escaping Reducer<StoreState>) {
         self.reducer = reducer
         self.state = state
     }
     
-    func dispatch(action: Action) {
+    public func dispatch(action: Action) {
         if let thunk = action as? Thunk<StoreState> {
             thunk.body(dispatch, getState)
         }
@@ -26,15 +26,15 @@ public class Store<StoreState: State> {
         subscribers.forEach { $0.new(state: state) }
     }
     
-    func subscribe(_ newSubscriber: StoreSubscriber) {
+    public func subscribe(_ newSubscriber: StoreSubscriber) {
         subscribers.append(newSubscriber)
     }
     
-    func getState() -> StoreState {
+    public func getState() -> StoreState {
         state
     }
 
-    func unsubscribe(_ storeSubscriber: StoreSubscriber) {
+    public func unsubscribe(_ storeSubscriber: StoreSubscriber) {
         guard let index = subscribers.firstIndex(where: { $0 as AnyObject === storeSubscriber as AnyObject }) else { return }
         
         subscribers.remove(at: index)
--- Sources/Redux/Thunk.swift
+++ Sources/Redux/Thunk.swift
@@ -9,4 +9,8 @@ public struct Thunk<S: State>: Action {
     
     let body: (_ dispatch: @escaping DispatchFunction, _ getState: @escaping () -> S?) -> Void
     
+    public init(body: @escaping (_ dispatch: @escaping DispatchFunction, _ getState: @escaping () -> S?) -> Void) {
+        self.body = body
+    }
+    
 }
